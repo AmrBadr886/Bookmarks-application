61b5d4de23326a072fa13a9eb178e0072a776b34
Amrtarek <owen_coool@yahoo.com>
2016-04-17 16:46:41 +0200

    login functionality added

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', main_page, name='main_page'),
-    url(r'^users/(?P<username>\w+)$', user_page, name='user_page')
+    url(r'^users/(?P<username>\w+)$', user_page, name='user_page'),
+    url(r'^login/$', user_login, name='user_login')
 ]
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 
 def main_page(request):
@@ -10,3 +13,24 @@ def main_page(request):
 def user_page(request, username):
     user = get_object_or_404(User, username=username)
     return render(request, 'bookmarks/user_page.html', {'user': user})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse('main_page'))
+            else:
+                error = 'invalid username or password'
+        else:
+            error = None
+    else:
+        form = LoginForm()
+        error = None
+    return render(request, 'bookmarks/user_login.html', {'form': form, 'error':error})
