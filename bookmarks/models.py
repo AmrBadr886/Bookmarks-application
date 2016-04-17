ebfe699c93f517fd595509f559a3eb815e43e790
Amrtarek <owen_coool@yahoo.com>
2016-04-17 15:01:52 +0200

    models created

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,3 +3,18 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+class Link(models.Model):
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
+
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
